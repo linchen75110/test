96793beb78f26b259d74052ddbf99e85c6672b61
chenlin <linchen75@126.com>
2017-10-24 10:40:32 +0800

    修改配置

--- config/config_ucenter.php
+++ config/config_ucenter.php
@@ -13,7 +13,7 @@ define('UC_DBCONNECT', 0);
 
 define('UC_CHARSET', 'utf-8');
 define('UC_KEY', 'D0tas5c5A4RaAcz3k198zci2cbf3gbIbx7z1Vbj0G3NdSb60IdmcPbL5CdV4wcK3');
-define('UC_API', 'http://shequ-test.mini1.cn/uc_server');
+define('UC_API', 'http://www.dz.com/uc_server');
 define('UC_APPID', '1');
 define('UC_IP', '');
 define('UC_PPP', 20);
